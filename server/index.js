07a171088483c293726f21c006c1e7e698713ebd
bakuwaku <emanukya@terpmail.umd.edu>
2025-05-18 12:45:59 -0400

    created an index.html, fixed some issues

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
 const cryptoRoutes = require('./routes/crypto');
 const portfolioRoutes = require('./routes/portfolio');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.use('/api/crypto', cryptoRoutes);
 app.use('/api/portfolio', portfolioRoutes);
